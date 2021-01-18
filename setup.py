6ba4eb6f44c7169a7a6384f8d0a546ab22aca235
Dave Leonard <david.leonard203@gmail.com>
2021-01-18 16:53:00 -0500

    Add version 0.0.2 markdown styling!

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 def readme():
     with open("README.md", "r") as f:
-        return f.read().encode("utf-8")
+        return f.read()
 
 setup(name="occasionally",
-        version="v0.0.1",
+        version="v0.0.2",
         # description="A task scheduling system implemented using only stdlib.",
         long_description=readme(),
-        long_description_content_type="text/markdown",
+        long_description_content_type='text/markdown',
         classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
